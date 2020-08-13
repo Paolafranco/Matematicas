63535b231ffdc4f5bcdd71535970624911f40881
Paola Franco <paoelizabeth69@gmail.com>
2020-08-13 17:06:15 -0500

    Matematicas

---
+++ Matematica.AppConsola/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Matematica.AppConsola
+{
+    internal class Calculadora
+    {
+        internal int[,] Multiplicar(int[,] matrizA, int[,] matrizB)
+        {
+            int filas = matrizA.GetLength(0);
+            int columnas = matrizB.GetLength(1);
+            int[,] matrizAxB = new int[filas, columnas];
+            int columnasA = matrizA.GetLength(1);
+
+            Parallel.For(0, filas, i =>
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    //Columnas de la matriz A
+                    int acumulador = 0;
+                    for (int k = 0; k < columnasA; k++)
+                    {
+                        //multiplicando y sumando
+                        acumulador += matrizA[i, k] * matrizB[k, j];
+                    }
+                    matrizAxB[i, j] = acumulador;
+                }
+            });
+            return matrizAxB;
+        }
+
+        internal int[,] CrearMatriz(int fila, int columna)
+        {
+            var generador = new Random();
+            int[,] matriz = new int[fila, columna];
+
+            Parallel.For(0, fila, i =>
+            {
+                for (int j = 0; j < columna; j++)
+                {
+                    matriz[i, j] = generador.Next(10, 99);
+                }
+            });
+            return matriz;
+        }
+    }
+}
---
+++ Matematica.AppConsola/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Matematica.AppConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] matrizA = new int[2, 3];
+            //matrizA[0, 0] = 1;
+            //matrizA[0, 1] = 2;
+            //matrizA[0, 2] = 4;
+            //matrizA[1, 0] = 2;
+            //matrizA[1, 1] = 5;
+            //matrizA[1, 2] = 8;
+
+            //int[,] matrizB = new int[3, 5];
+            //matrizB[0, 0] = 4;
+            //matrizB[0, 1] = 9;
+            //matrizB[0, 2] = 2;
+            //matrizB[0, 3] = 7;
+            //matrizB[0, 4] = 3;
+
+            //matrizB[1, 0] = 1;
+            //matrizB[1, 1] = 3;
+            //matrizB[1, 2] = 3;
+            //matrizB[1, 3] = 4;
+            //matrizB[1, 4] = 7;
+
+            //matrizB[2, 0] = 6;
+            //matrizB[2, 1] = 5;
+            //matrizB[2, 2] = 4;
+            //matrizB[2, 3] = 1;
+            //matrizB[2, 4] = 9;
+
+            Calculadora calculadora = new Calculadora();
+            int[,] matrizA = calculadora.CrearMatriz(20, 30);
+            int[,] matrizB = calculadora.CrearMatriz(30, 40);
+            var watch = Stopwatch.StartNew();
+            int[,] matrizAxB = calculadora.Multiplicar(matrizA, matrizB);
+            watch.Stop();
+            var ticksTranscurridos = watch.ElapsedTicks;
+            Console.WriteLine($"El tiempo trancurrido fue de : {ticksTranscurridos}");
+
+            for (int i = 0; i < matrizAxB.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < matrizAxB.GetLength(1); j++)
+                {
+                    Console.Write($"{matrizAxB[i, j]}\t");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
